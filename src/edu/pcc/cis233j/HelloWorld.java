d6360ae5934dc1c05f171c32bc38b1a6a5eceecc
Alex Leykand <alexander.leykand@pcc.edu>
2020-01-28 20:28:06 -0800

    added printout of the timestamp

--- src/edu/pcc/cis233j/HelloWorld.java
+++ src/edu/pcc/cis233j/HelloWorld.java
@@ -1,12 +1,13 @@
 package edu.pcc.cis233j;
+import java.util.Date;
 /*  This is a class created for Lab 4 (working with GitHub).
  *
  * @author  Alex Leykand
  * @version 2020.01.28
  */
-
 public class HelloWorld {
  public static void main(String [] args){
      System.out.println("Hello, world!");
+     System.out.println(new Date());
  }
 }
