e7b83341fa5e9350598166436b55e42ce691f1ce
Alex Leykand <alexander.leykand@pcc.edu>
2020-01-28 20:22:44 -0800

    added main method and Hello, world! message

--- src/edu/pcc/cis233j/HelloWorld.java
+++ src/edu/pcc/cis233j/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis233j;
+/*  This is a class created for Lab 4 (working with GitHub).
+ *
+ * @author  Alex Leykand
+ * @version 2020.01.28
+ */
 
 public class HelloWorld {
+ public static void main(String [] args){
+     System.out.println("Hello, world!");
+ }
 }
